b4843fffaf6fafb19adabc51e9aa490990b024c0
yaoming <yaoming@atguigu.com>
2019-01-05 17:40:54 +0800

    第一次

---
+++ src/main/java/com/atguigu/git/entity/Employee.java
@@ -0,0 +1,10 @@
+package com.atguigu.git.entity;
+
+public class Employee {
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbb");
+        System.out.println("ccccccccc");
+    }
+}
